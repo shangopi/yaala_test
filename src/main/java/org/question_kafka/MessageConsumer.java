b371d72e80dbbce10ad3fc0f927c5d17b5ef05b7
shangopi <gopinath.19@cse.mrt.ac.lk>
2024-06-06 13:52:32 +0530

    since poll() is deprecated .. changing to poll(mills) method

--- src/main/java/org/question_kafka/MessageConsumer.java
+++ src/main/java/org/question_kafka/MessageConsumer.java
@@ -7,6 +7,7 @@ import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.serialization.IntegerDeserializer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 
+import java.time.Duration;
 import java.util.List;
 import java.util.Properties;
 
@@ -21,13 +22,14 @@ public class MessageConsumer {
         configProps.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
         configProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, IntegerDeserializer.class.getName());
         configProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        configProps.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest"); // Reset to latest offset if no committed offset is found
 
         try (KafkaConsumer<Integer, String> consumer = new KafkaConsumer<>(configProps)) {
             // Subscribe to topics
             consumer.subscribe(List.of(topicName));
             System.out.println("Listening for messages...");
             while (true) {
-                ConsumerRecords<Integer, String> consumerRecords = consumer.poll(pollingTime);
+                ConsumerRecords<Integer, String> consumerRecords = consumer.poll(Duration.ofMillis(pollingTime));
                 for (ConsumerRecord<Integer, String> consumerRecord : consumerRecords) {
                     System.out.printf("Received message: key: %d, value: %s%n", consumerRecord.key(), consumerRecord.value());
                 }
